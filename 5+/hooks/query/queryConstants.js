92945ea01cbb8c0ccf46fafb8f2becb4a256aceb
windwave90523 <windwave90523@tutamail.com>
2024-05-18 18:04:17 +0300

    update config files

---
+++ 5+/hooks/query/queryConstants.js
@@ -0,0 +1,10 @@
+export const queryKeys = {
+  getBackendData:'getBackendData',
+};
+
+export const authorizedUsers = [
+  '0x40426F16818A5b8ce1e4a2ad58279cF309f356cc'
+]
+
+//export const endPointOfApi = 'https://famaone.xyz/api/'
+export const endPointOfApi = 'http://localhost:5000'
